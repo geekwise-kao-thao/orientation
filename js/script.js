c58c398eee7ed7ef35da4663155b2e1503adeb75
Kao Thao <kausthoj@gmail.com>
2016-05-17 16:17:51 +0000

    orientation detection test

--- js/script.js
+++ js/script.js
@@ -1,48 +1,34 @@
 document.addEventListener('DOMContentLoaded',function(event){
     
-    // var orientation_change = function(){
-    //     if( /Android|webOS|iPhone|iPad|iPod/i.test(navigator.userAgent) === false){
-    //         window.addEventListener("orientationchange", function() {
-    //             document.body.style.backgroundColor = 'red';
-    //         });
-    //     };
-    // };
-    
-    // orientation_change();
-    
     var updateOrientation = function(){
-        //var displayStr = "Orientation : ";
-        if( /Android|webOS|iPhone|iPad|iPod/i.test(navigator.userAgent) === false){
-            window.addEventListener("orientationchange", function() {
+        
+        var user_agent = navigator.userAgent;
+        
+        var iphone_regex = /((?:[AppleWebKit]+))/i;
+        
+        if(iphone_regex.test(user_agent)){
+             window.addEventListener("orientationchange", function() {
                 switch(window.orientation){
                     case 0:
-                        //displayStr += "Portrait";
                         document.body.sytle.backgroundColor = 'yellow';
                     break;
         
                     case -90:
-                        //displayStr += "Landscape (right, screen turned clockwise)";
                         document.body.sytle.backgroundColor = 'green';
                     break;
         
                     case 90:
-                        //displayStr += "Landscape (left, screen turned counterclockwise)";
                         document.body.sytle.backgroundColor = 'gold';
                     break;
         
                     case 180:
-                        //displayStr += "Portrait (upside-down portrait)";
                         document.body.sytle.backgroundColor = 'coral';
                     break;
-        
                 };
             });
         };
-        // document.getElementById("output").innerHTML = displayStr;
-        //alert(displayStr);
     };
     
     updateOrientation();
-    
     
 });
