2dafc63b66a63f45a218d7565b6135942ba26896
Kao Thao <kausthoj@gmail.com>
2016-05-17 17:53:14 +0000

    orientation detection test seven

--- js/script.js
+++ js/script.js
@@ -1,13 +1,41 @@
-var user_agent = navigator.userAgent;
+// var user_agent = window.navigator.userAgent;
 
-var iphone_regex = /(^Mozilla+)/gi;
+// var iphone_regex = /(^Mozilla+)/gi;
 
-var updateOrientation = function(){
+// var updateOrientation = function(){
 
-    if(user_agent == iphone_regex){
+//     if(user_agent.toString(iphone_regex)){
         
-         window.addEventListener("orientationchange", function() {
-            switch(window.orientation){
+//          window.addEventListener("orientationchange", function() {
+//             switch(window.orientation){
+//                 case 0:
+//                     document.body.sytle.backgroundColor = 'yellow';
+//                 break;
+    
+//                 case -90:
+//                     document.body.sytle.backgroundColor = 'blue';
+//                 break;
+    
+//                 case 90:
+//                     document.body.sytle.backgroundColor = 'gold';
+//                 break;
+    
+//                 case 180:
+//                     document.body.sytle.backgroundColor = 'coral';
+//                 break;
+//             };
+//         });
+//     };
+// };
+
+//document.addEventListener('DOMContentLoaded',function(event){
+    
+    // document.body.style.backgroundColor = 'lightgreen';
+    // updateOrientation();
+    
+    window.addEventListener("orientationchange", function() {
+    alert("the orientation of the device is now " + screen.orientation.angle);
+    switch(window.orientation){
                 case 0:
                     document.body.sytle.backgroundColor = 'yellow';
                 break;
@@ -24,13 +52,6 @@ var updateOrientation = function(){
                     document.body.sytle.backgroundColor = 'coral';
                 break;
             };
-        });
-    };
-};
-
-document.addEventListener('DOMContentLoaded',function(event){
-    
-    document.body.style.backgroundColor = 'lightgreen';
-    updateOrientation();
-    
 });
+    
+//});
